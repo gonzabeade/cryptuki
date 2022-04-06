76b3f667b0576e01b807b263827725412856f7c0
Gonza Beade <gbeade@itba.edu.ar>
2022-04-06 01:30:20 -0300

    Finished entity modelling

--- models/src/main/java/ar/edu/itba/paw/User.java
+++ models/src/main/java/ar/edu/itba/paw/User.java
@@ -1,22 +1,28 @@
 package ar.edu.itba.paw;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
 
     private int id;
     private String email;
     private int ratingSum;
     private int ratingCount;
+    private Set<Role> roles;
 
     public class Builder {
         private int id;
         private String email = "";
         private int ratingSum = 0;
         private int ratingCount = 0;
+        private Set<Role> roles = new HashSet<>();
 
         public Builder(int id) { this.id = id; }
         public Builder email(String email) {this.email = email; return this;}
         public Builder ratingSum(int sum) {this.ratingSum = sum; return this; }
         public Builder ratingCount(int count) {this.ratingCount = count; return this; }
+        public Builder addRole(Role role) { roles.add(role); return this; }
 
         public User build() {return new User(this);}
     }
@@ -26,6 +32,7 @@ public class User {
         this.email = builder.email;
         this.ratingCount = builder.ratingCount;
         this.ratingSum = builder.ratingSum;
+        this.roles = builder.roles;
     }
 
     public int getId() {
@@ -40,6 +47,6 @@ public class User {
     public int getRatingCount() {
         return ratingCount;
     }
-
+    public Set<Role> getRoles() { return roles; }
 
 }

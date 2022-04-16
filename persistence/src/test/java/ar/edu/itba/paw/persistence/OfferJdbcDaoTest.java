30e8c4fc4f98d3212400542a844d5f73aecb915b
salvaCasta <salCas276@gmail.com>
2022-04-16 17:07:26 -0300

    Added lists for testing OfferDao

--- persistence/src/test/java/ar/edu/itba/paw/persistence/OfferJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/OfferJdbcDaoTest.java
@@ -13,20 +13,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 //TODO: script sql especial para poblar la bd con tablas de las que dependa offer, por fk por ejemplo
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class OfferJdbcDaoTest {
 
-    private static final String USER_MAIL = "scastagnino@itba.edu.ar";
-    private static final int USER_ID = 1;
     private static final String OFFER_TABLE = "offer";
-    private static final String CRYPTO_NAME = "BTC";
-    private static final double MARKET_PRICE = 22.1;
-    private static final String CRYPTO_CODE = "1";
-    private static final double ASKING_PRICE = 20.65;
-    private static final int OFFER_ID = 1;
+    private ArrayList<Offer> offers;
 
     @Autowired
     private DataSource ds;
@@ -43,6 +40,21 @@ public class OfferJdbcDaoTest {
                 .usingGeneratedKeyColumns("id");
         offerJdbcDao = new OfferJdbcDao(ds);
         JdbcTestUtils.deleteFromTables(jdbcTemplate,"offers");
+
+        offers = new ArrayList<>(Arrays.asList(
+                offerJdbcDao.makeOffer(Offer.builder(User.builder().id(1).email("scastagnino@itba.edu.ar").build(),
+                        Cryptocurrency.getInstance("1", "BTC", 22.1),
+                        22.2)),
+                offerJdbcDao.makeOffer(Offer.builder(User.builder().id(2).email("shadad@itba.edu.ar").build(),
+                        Cryptocurrency.getInstance("3", "ADA", 10.4),
+                        9)),
+                offerJdbcDao.makeOffer(Offer.builder(User.builder().id(3).email("gbeade@itba.edu.ar").build(),
+                        Cryptocurrency.getInstance("2", "ETH", 54.3),
+                        54.0)),
+                offerJdbcDao.makeOffer(Offer.builder(User.builder().id(4).email("mdedeu@itba.edu.ar").build(),
+                        Cryptocurrency.getInstance("4", "DOT", 2.0),
+                        2.65))
+        ));
     }
 
     @Test
@@ -53,36 +65,56 @@ public class OfferJdbcDaoTest {
         //TODO: estas 3 lineas de abajo en las que creo un builder (usando modelos) que voy a usar para el Dao, van en setup?
 
         //TODO: ver de agregar campos opcionales al builder
-        User user = User.builder().id(USER_ID).email(USER_MAIL).build();
-        Cryptocurrency crypto = Cryptocurrency.getInstance(CRYPTO_CODE, CRYPTO_NAME, MARKET_PRICE);
+
         //TODO: cuando se buildea una offer se esta craeando un Builder de la offer, este guarda el horario actual,
-        //TODO: puede haber algun problema con el tema del terminismo (ya que las horas sondiferentes)?
-        Offer.Builder offerBuilder = Offer.builder(user, crypto, ASKING_PRICE);
+        //TODO: puede haber algun problema con el tema del determinismo (ya que las horas son diferentes)?
+        Offer.Builder offerBuilder = Offer.builder(offers.get(1).getSeller(), offers.get(1).getCrypto(), offers.get(1).getAskingPrice());
 
         // 2. ejercitar
-        Offer offer = offerJdbcDao.makeOffer(offerBuilder);
+        Offer testedOffer = offerJdbcDao.makeOffer(offerBuilder);
 
         // 3. validaciones
-        Assert.assertNotNull(offer);
+        Assert.assertNotNull(testedOffer);
         //TODO: Juan valida que el User se haya creado correctamente, el tema es que en esta caso crar la Offer es muy parecido a por ejemplo crear una Crypto
         //TODO: y esto lo hice en setup ya que son modelos, deberia validar esto entonces, y la creacion de lo que esta en setup?
 
         //TODO: mirar que hacer con metodos deprecados, hay que agregar delta
-        Assert.assertEquals(ASKING_PRICE, offer.getAskingPrice());
+        Assert.assertEquals(offers.get(1).getAskingPrice(), testedOffer.getAskingPrice());
 
-        Assert.assertEquals(USER_MAIL, offer.getSeller().getEmail());
-        Assert.assertEquals(USER_ID, offer.getSeller().getId());
+        Assert.assertEquals(offers.get(1).getSeller().getEmail(), testedOffer.getSeller().getEmail());
+        Assert.assertEquals(offers.get(1).getSeller().getId(), testedOffer.getSeller().getId());
 
-        Assert.assertEquals(MARKET_PRICE, offer.getCrypto().getMarketPrice());
-        Assert.assertEquals(CRYPTO_CODE, offer.getCrypto().getCode());
-        Assert.assertEquals(CRYPTO_NAME, offer.getCrypto().getName());
+        Assert.assertEquals(offers.get(1).getCrypto().getMarketPrice(), testedOffer.getCrypto().getMarketPrice());
+        Assert.assertEquals(offers.get(1).getCrypto().getCode(), testedOffer.getCrypto().getCode());
+        Assert.assertEquals(offers.get(1).getCrypto().getName(), testedOffer.getCrypto().getName());
 
         Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, OFFER_TABLE));
     }
 
     @Test
     public void TestGetAllOffers(){
-        //TODO: ver si poner un sql aparte para
+        // 1
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "offer");
+
+        // 2
+        List<Offer> testedOffers = offerJdbcDao.getAllOffers();
+
+        // 3
+        Assert.assertNotNull(testedOffers);
+        for (Offer testedOffer : testedOffers)
+            Assert.assertNotNull(testedOffer);
+        Assert.assertEquals(offers.size(), testedOffers.size());
+
+        for(int i = 0; i < offers.size(); i++) {
+            Assert.assertEquals(offers.get(i).getAskingPrice(), testedOffers.get(i).getAskingPrice());
+
+            Assert.assertEquals(offers.get(i).getSeller().getEmail(), testedOffers.get(i).getSeller().getEmail());
+            Assert.assertEquals(offers.get(i).getSeller().getId(), testedOffers.get(i).getSeller().getId());
+
+            Assert.assertEquals(offers.get(i).getCrypto().getMarketPrice(), testedOffers.get(i).getCrypto().getMarketPrice());
+            Assert.assertEquals(offers.get(i).getCrypto().getCode(), testedOffers.get(i).getCrypto().getCode());
+            Assert.assertEquals(offers.get(i).getCrypto().getName(), testedOffers.get(i).getCrypto().getName());
+        }
     }
 
     @Test
@@ -93,18 +125,18 @@ public class OfferJdbcDaoTest {
         //TODO: mirar como le seteo el user id, que en la original es un serial, lo pongo como default en 1
 
         // 2
-        Offer offer = offerJdbcDao.getOffer(OFFER_ID);
+        Offer testedOffer = offerJdbcDao.getOffer(1);
 
         // 3
-        Assert.assertEquals(OFFER_ID, offer.getId());
-        Assert.assertEquals(ASKING_PRICE, offer.getAskingPrice());
+        Assert.assertEquals(offers.get(1).getAskingPrice(), testedOffer.getAskingPrice());
 
-        Assert.assertEquals(USER_ID, offer.getSeller().getId());
-        Assert.assertEquals(USER_MAIL, offer.getSeller().getEmail());
+        Assert.assertEquals(offers.get(1).getSeller().getEmail(), testedOffer.getSeller().getEmail());
+        Assert.assertEquals(offers.get(1).getSeller().getId(), testedOffer.getSeller().getId());
 
-        Assert.assertEquals(MARKET_PRICE, offer.getCrypto().getMarketPrice());
-        Assert.assertEquals(CRYPTO_CODE, offer.getCrypto().getCode());
-        Assert.assertEquals(CRYPTO_NAME, offer.getCrypto().getName());
+        Assert.assertEquals(offers.get(1).getCrypto().getMarketPrice(), testedOffer.getCrypto().getMarketPrice());
+        Assert.assertEquals(offers.get(1).getCrypto().getCode(), testedOffer.getCrypto().getCode());
+        Assert.assertEquals(offers.get(1).getCrypto().getName(), testedOffer.getCrypto().getName());
+
     }
 
 

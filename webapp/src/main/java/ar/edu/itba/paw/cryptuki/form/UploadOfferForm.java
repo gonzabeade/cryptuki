5c398abfc8505db219ce443e3363cef7a10f1391
mdedeu <mdedeu@itba.edu.ar>
2022-04-30 21:34:25 -0300

    Filter selected value, weird order in upload_offer and Location added to form

--- webapp/src/main/java/ar/edu/itba/paw/cryptuki/form/UploadOfferForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/cryptuki/form/UploadOfferForm.java
@@ -28,6 +28,19 @@ public class UploadOfferForm {
 
     @Size(min = 1)
     private String[] paymentMethods;
+
+    @Size(max = 40)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
     public Float getMinAmount() {
         return minAmount;
     }

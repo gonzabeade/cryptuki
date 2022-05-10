12f7b994b5a39419a4b062eaacbfeb5faa5bb8b3
salvaCasta <salCas276@gmail.com>
2022-05-10 06:06:47 -0300

    Added NeedHelp and Question mail messages

--- service/src/main/java/ar/edu/itba/paw/service/mailing/QuestionThymeleafMailMessage.java
+++ service/src/main/java/ar/edu/itba/paw/service/mailing/QuestionThymeleafMailMessage.java
@@ -2,24 +2,21 @@ package ar.edu.itba.paw.service.mailing;
 
 import org.thymeleaf.context.Context;
 
-public class NeedHelpThymeleafMailMessage extends ThymeleafMailMessage {
-
-    private final static String template = "NeedHelpTemplate";
+public class QuestionThymeleafMailMessage extends ThymeleafMailMessage{
+    private final static String template = "QuestionTemplate";
 
     private String username;
     private String question;
-    private String answer;
 
-    public NeedHelpThymeleafMailMessage(String from, String to) {
+    public QuestionThymeleafMailMessage(String from, String to) {
         super(from, to, template);
     }
-    public NeedHelpThymeleafMailMessage(MailMessage mailMessage) {
+    public QuestionThymeleafMailMessage(MailMessage mailMessage) {
         super(mailMessage, template);
     }
 
 
-    public void setParameters(String username, String question, String answer) {
-        this.answer = answer;
+    public void setParameters(String username, String question) {
         this.question = question;
         this.username = username;
     }
@@ -27,23 +24,21 @@ public class NeedHelpThymeleafMailMessage extends ThymeleafMailMessage {
     @Override
     protected Context getContext() {
 
-        if ( username == null || question == null || answer == null )
+        if ( username == null || question == null)
             throw new IllegalStateException("Cannot send email with missing parameters");
 
         Context context = new Context();
-        context.setVariable("staticTitle", "¡Consulta Contestada!");
+        context.setVariable("staticTitle", "¡Consulta Recibida!");
         context.setVariable("staticHello", "Hola!");
-        context.setVariable("staticQuestionReceived", "Hemos recibido tu consulta.");
+        context.setVariable("staticQuestionReceived", "Hemos recibido tu consulta");
+        context.setVariable("staticWillAnswer", "Esta será contestada a la brevedad por nuestro equipo.");
         context.setVariable("staticYourQuestion", "Tu consulta");
-        context.setVariable("staticAnswer", "Respuesta");
-        context.setVariable("staticRewriteUs", "En caso de que la respuesta no sea lo que estabas buscando podrás volver a escribirnos.");
         context.setVariable("staticNeedMoreHelp", "¿Necesitas más ayuda?");
         context.setVariable("staticWriteUs", "Escríbenos");
 
 
         context.setVariable("question", question);
         context.setVariable("username", username);
-        context.setVariable("answer", answer);
         return context;
     }
 }

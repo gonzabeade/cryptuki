9ec371439b175987b184a6f6451c5817b85bb4f2
Gonza Beade <gbeade@itba.edu.ar>
2022-05-11 01:37:48 -0300

    Amount check

--- webapp/src/main/java/ar/edu/itba/paw/cryptuki/form/validator/AmountValidator.java
+++ webapp/src/main/java/ar/edu/itba/paw/cryptuki/form/validator/AmountValidator.java
@@ -5,35 +5,49 @@ import ar.edu.itba.paw.cryptuki.form.OfferBuyForm;
 import ar.edu.itba.paw.cryptuki.form.annotation.EqualFields;
 import ar.edu.itba.paw.persistence.Offer;
 import ar.edu.itba.paw.service.OfferService;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
-public class AmountValidator implements ConstraintValidator<AmountCheck, Float> {
+public class AmountValidator implements ConstraintValidator<AmountCheck, Object> {
 
     @Autowired
     private OfferService offerService;
 
+    private String amount;
     private String offerId;
 
     public void initialize(AmountCheck constraintAnnotation) {
+        this.amount = constraintAnnotation.amount();
         this.offerId = constraintAnnotation.offerId();
     }
 
     @Override
-    public boolean isValid(Float amount, ConstraintValidatorContext constraintValidatorContext) {
-        Integer offerId = Integer.getInteger(this.offerId);
-        if (offerId == null)
-            return false;
-        Optional<Offer> offerOptional = offerService.getOfferById(offerId);
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        Integer offerIdInt;
+        Float amountNumber;
+        try {
+            offerIdInt = (Integer) new BeanWrapperImpl(value).getPropertyValue(offerId);
+        } catch (ClassCastException classCastException) {
+            throw new IllegalArgumentException("offerId must be an Integer.", classCastException);
+        }
+        try {
+            amountNumber = (Float) new BeanWrapperImpl(value).getPropertyValue(amount);
+        } catch (ClassCastException classCastException) {
+            throw new IllegalArgumentException("amount must be a Float.", classCastException);
+        }
+
+        Optional<Offer> offerOptional = offerService.getOfferById(offerIdInt);
         if (!offerOptional.isPresent())
             return false;
         Offer offer = offerOptional.get();
         float offerPrice = offer.getAskingPrice();
-        return ( amount != null && offerPrice * offer.getMinQuantity() <= amount && amount <= offerPrice * offer.getMaxQuantity());
+        return ( amountNumber != null && amountNumber.compareTo(offerPrice * offer.getMinQuantity()) >= 0  && amountNumber.compareTo(offerPrice * offer.getMaxQuantity()) <= 0);
     }
 }

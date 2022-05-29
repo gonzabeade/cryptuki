471b4afa86cdd1c7147c075ed886d0d31d232c81
shadad00 <shadad00@itba.edu.ar>
2022-05-29 11:21:38 -0300

    Login bug fixed

--- models/src/main/java/ar/edu/itba/paw/persistence/User.java
+++ models/src/main/java/ar/edu/itba/paw/persistence/User.java
@@ -22,7 +22,7 @@ public final class User {
     private  int ratingSum;
     @Column(name="rating_count",nullable = false)
     private  int ratingCount;
-    @Column(name="rating", nullable = false)
+    @Column(name="rating", nullable = false,insertable = false,updatable = false)
     private float rating;
 
     @Column(name="last_login")

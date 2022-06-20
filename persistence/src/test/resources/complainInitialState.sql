bb3bf8fdee29cdfe2aa146110598c05e3e92d4c7
salvaCast276 <salCas276@gmail.com>
2022-06-20 18:58:54 -0300

    Continued with complain tests

--- persistence/src/test/resources/complainInitialState.sql
+++ persistence/src/test/resources/complainInitialState.sql
@@ -20,9 +20,9 @@ INSERT INTO auth (status, code, role_id, user_id, uname, password) VALUES (0, 43
 INSERT INTO cryptocurrency (code, market_price, commercial_name) VALUES ('BTC', 22.1, 'Bitcoin');
 INSERT INTO cryptocurrency (code, market_price, commercial_name) VALUES ('ETH', 54.3, 'Ether');
 
-INSERT INTO offer (offer_id,seller_id, offer_date, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (0,0, '2020-11-01 01:18:33', 'BTC', 'APR', 10.33, 4.2, 2.6, 'Im selling BTC', 'CABA');
-INSERT INTO offer (offer_id,seller_id, offer_date, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (1,0, '2003-04-27 21:03:02', 'ETH', 'APR', 6, 10.7, 1, 'Im selling ETH', 'La Plata');
-INSERT INTO offer (offer_id,seller_id, offer_date, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (2,1, '2001-06-27 22:05:02', 'ETH', 'APR', 6.5, 16.7, 0.4, 'Im selling ETH', 'Pilar');
+INSERT INTO offer (offer_id,seller_id, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (0,0, 'BTC', 'APR', 10.33, 4.2, 2.6, 'Im selling BTC', 'CABA');
+INSERT INTO offer (offer_id,seller_id, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (1,0, 'ETH', 'APR', 6, 10.7, 1, 'Im selling ETH', 'La Plata');
+INSERT INTO offer (offer_id,seller_id, crypto_code, status_code, asking_price, max_quantity, min_quantity, comments, location) VALUES (2,1, 'ETH', 'APR', 6.5, 16.7, 0.4, 'Im selling ETH', 'Pilar');
 
 INSERT INTO trade (trade_id,offer_id, buyer_id, seller_id, quantity) VALUES (0,0, 1, 0, 3.4);
 INSERT INTO trade (trade_id ,offer_id, buyer_id, seller_id, quantity) VALUES (1,0, 1, 0, 5.6);

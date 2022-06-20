2908ead60139056b66135b75ccc4f5f09feb62be
salvaCast276 <salCas276@gmail.com>
2022-06-20 08:43:19 -0300

    Finished almost all userService tests, missing just one test

--- service/src/test/java/ar/edu/itba/paw/service/TradeServiceImplTest.java
+++ service/src/test/java/ar/edu/itba/paw/service/TradeServiceImplTest.java
@@ -26,15 +26,8 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class TradeServiceImplTest {
 
-
-    @Mock
-    private  OfferService offerService;
     @Mock
     private  TradeDao tradeDao;
-    @Mock
-    private  UserAuthDao userAuthDao;
-    @Mock
-    private  MessageSenderFacade messageSenderFacade;
     @Mock
     private UserService userService;
 
@@ -82,25 +75,5 @@ public class TradeServiceImplTest {
 
         tradeService.rateCounterPartUserRegardingTrade(sellerAuth.getUsername(), 12, 0);
     }
-
-//    //TODO:SALVA tests viejos de salta supongo
-//    @Test(expected = NoSuchOfferException.class)
-//    public void makeTradeWithInvalidOffer(){
-////        Trade.Builder trade = new Trade.Builder(0,"shadad");
-////        when(offerService.getOfferById(0)).thenReturn(Optional.empty());
-////        tradeService.makeTrade(trade);
-//    }
-//
-//    @Test(expected = NoSuchUserException.class)
-//    public void makeTradeWithInvalidUserAuth(){
-////        Trade.Builder trade = new Trade.Builder(0,"shadad");
-////        UtilsClass.UserPublic user = (UtilsClass.UserPublic)new UtilsClass.UserPublic("shadad@itba.edu.ar").withId(5);
-////        UtilsClass.OfferPublic offer =(UtilsClass.OfferPublic) new UtilsClass.OfferPublic(0,user.build(),null,18.5f).withMinQuantity(0).withMaxQuantity(5);
-////
-////        when(offerService.getOfferById(0)).thenReturn(Optional.of(offer.build()));
-////        when(userAuthDao.getUserAuthByEmail("shadad@itba.edu.ar")).thenReturn(Optional.empty());
-////        tradeService.makeTrade(trade);
-//    }
-
 
 }
--- service/src/test/java/ar/edu/itba/paw/service/UserServiceImplTest.java
+++ service/src/test/java/ar/edu/itba/paw/service/UserServiceImplTest.java
@@ -1,70 +1,79 @@
 package ar.edu.itba.paw.service;
 
+import ar.edu.itba.paw.exception.NoSuchUserException;
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.UserAuth;
 import ar.edu.itba.paw.model.UserStatus;
 import ar.edu.itba.paw.persistence.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.swing.text.html.Option;
+import java.util.Locale;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
 
-
     @Mock
     private UserAuthDao userAuthDao;
     @Mock
     private UserDao userdao;
-
     @Mock
     private MessageSenderFacade messageSenderFacade;
-
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    private static String username="shadad";
-    private static String password ="pass" ;
-    private static Integer code = 355;
+    @InjectMocks
+    private UserServiceImpl userService;
 
-
-//    @InjectMocks
-//    private UserService userService = new UserServiceImpl(userdao,userAuthDao,passwordEncoder,messageSenderFacade);
+    private User user = new User("salvaCasta@gmail.com", "12345678", 7, 58, Locale.forLanguageTag("en-US"));
+    private UserAuth userAuth = new UserAuth(user.getId(), "salvaCasta", "castaSalva", 0);
 
 
     @Test
-    public void BadCodechangePasswordTest()  {
-//        UtilsClass.UserAuthPublic userAuthPublic = (UtilsClass.UserAuthPublic) new UtilsClass.UserAuthPublic(username,password).withCode(code).withId(0).withRole("seller").withUserStatus(UserStatus.UNVERIFIED);
-//        when(userAuthDao.getUserAuthByUsername(username)).thenReturn(
-//                Optional.of(userAuthPublic.build())
-//        );
+    public void testSuccessCodeChangePasswordTest()  {
+        user.setUserAuth(userAuth);
+        when(userAuthDao.getUserAuthByUsername(anyString())).thenReturn(Optional.of(userAuth));
+        when(userAuthDao.changePassword(anyString(), anyString())).thenReturn(true);
 
-//        boolean answer = userService.changePassword(username,code+1,password);
+        boolean changed = userService.changePassword(userAuth.getUsername(), userAuth.getCode(), "newPassword");
 
-//        Assert.assertEquals(false,answer);
+        Assert.assertTrue(changed);
     }
 
     @Test
-    public void SuccessCodechangePasswordTest()  {
-//        UtilsClass.UserAuthPublic userAuthPublic = (UtilsClass.UserAuthPublic) new UtilsClass.UserAuthPublic(username,password).withCode(code).withId(0).withRole("seller").withUserStatus(UserStatus.UNVERIFIED);
-//        when(userAuthDao.getUserAuthByUsername(username)).thenReturn(
-//                Optional.of(userAuthPublic.build())
-//        );
-//        when(passwordEncoder.encode(password)).thenReturn(password);
-//        when(userAuthDao.changePassword(username,password)).thenReturn(true);
-//
-//        boolean answer = false; // userService.changePassword(username,code,password);
-//
-//        Assert.assertEquals(true,answer);
+    public void testErroneousCodeChangePasswordTest()  {
+        user.setUserAuth(userAuth);
+        when(userAuthDao.getUserAuthByUsername(anyString())).thenReturn(Optional.of(userAuth));
+        when(userAuthDao.changePassword(anyString(), anyString())).thenReturn(true);
+
+        boolean changed = userService.changePassword(userAuth.getUsername(), userAuth.getCode()+1, "newPassword");
+
+        Assert.assertFalse(changed);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidCodeChangePassword()  {
 
+        boolean changed = userService.changePassword(userAuth.getUsername(), -1, "newPassword");
 
+    }
 
+    @Test(expected = NoSuchUserException.class)
+    public void testInvalidAnonymouslChangePassword() {
+        userAuth.setUserStatus(UserStatus.UNVERIFIED);
+        when(userAuthDao.getUserAuthByEmail(anyString())).thenReturn(Optional.of(userAuth));
+
+        userService.changePasswordAnonymously(user.getEmail());
+    }
 
 }

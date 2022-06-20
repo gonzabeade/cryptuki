85067c9b67f7ac8229b160d4ea6e2d42f2cf7310
salvaCast276 <salCas276@gmail.com>
2022-06-20 00:54:22 -0300

    Started testing TradeService

--- service/src/test/java/ar/edu/itba/paw/service/TradeServiceImplTest.java
+++ service/src/test/java/ar/edu/itba/paw/service/TradeServiceImplTest.java
@@ -2,34 +2,68 @@ package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.exception.NoSuchOfferException;
 import ar.edu.itba.paw.exception.NoSuchUserException;
+import ar.edu.itba.paw.model.Offer;
 import ar.edu.itba.paw.model.Trade;
+import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.model.UserAuth;
 import ar.edu.itba.paw.persistence.TradeDao;
 import ar.edu.itba.paw.persistence.UserAuthDao;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Locale;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TradeServiceImplTest {
-//
-//    @Mock
-//    private  OfferService offerService;
-//    @Mock
-//    private  TradeDao tradeDao;
-//    @Mock
-//    private  UserAuthDao userAuthDao;
-//    @Mock
-//    private  MessageSenderFacade messageSenderFacade;
-//
-//    @InjectMocks
-////    private TradeServiceImpl tradeService = new TradeServiceImpl(offerService,tradeDao,userAuthDao,messageSenderFacade);
-//
+
+    private User user = new User("salvaCasta@gmail.com", "12345678", 7, 58, Locale.forLanguageTag("en-US"));
+    private UserAuth userAuth = new UserAuth(user.getId(), "salvaCasta", "castaSalva", 0);
+
+    @Mock
+    private  OfferService offerService;
+    @Mock
+    private  TradeDao tradeDao;
+    @Mock
+    private  UserAuthDao userAuthDao;
+    @Mock
+    private  MessageSenderFacade messageSenderFacade;
+    @Mock
+    private UserService userService;
+    @Mock
+    private Offer offer = new Offer.Builder(10, 50,100).build();
+    @Mock
+    private Trade trade = new Trade(offer, user, 20);
+
+
+    @InjectMocks
+    private TradeServiceImpl tradeService;
+
+    @Test
+    public void testRateCounterpartAsBuyer(){
+        user.setUserAuth(userAuth);
+        when(tradeDao.getTradeById(anyInt())).thenReturn(Optional.of(trade));
+        when(offer.getSeller()).thenReturn(user);
+        when(trade.getBuyer()).thenReturn(user);
+        when(trade.getOffer()).thenReturn(offer);
+        when(trade.markSellerAsRated()).
+        doNothing().when(userService).updateRatingBy(anyString(), anyInt());
+
+        tradeService.rateCounterPartUserRegardingTrade(userAuth.getUsername(), 8, 0);
+
+        Assert.assertTrue(trade.isSellerRated());
+    }
+
+    //TODO:SALVA tests viejos de salta supongo
 //    @Test(expected = NoSuchOfferException.class)
 //    public void makeTradeWithInvalidOffer(){
 ////        Trade.Builder trade = new Trade.Builder(0,"shadad");

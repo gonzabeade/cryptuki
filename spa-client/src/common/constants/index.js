19d1f7f1b6236368c9f5a1760e9b280443184674
Gonza Beade <gbeade@itba.edu.ar>
2022-08-25 15:26:53 -0300

    Begin frontend structure

---
+++ spa-client/src/common/constants/index.js
@@ -0,0 +1,20 @@
+export const paths = {
+    LOCAL_BASE_URL : 'http://localhost:3000/webapp',
+    BASE_URL: 'http://localhost:8080/webapp/api',
+    OFFERS: '/offers',
+    TRADES: '/trades',
+    USERS: '/users',
+    COMPLAINTS: '/complaints',
+    LOCATIONS: '/locations',
+    CRYPTOCURRENCIES: '/cryptocurrencies',
+    TIME_TABLE: '/timetable',
+}
+  
+// export const APPLICATION_V1_JSON_TYPE = 'application/vnd.campus.api.v1+json'
+  
+export const userRoles = {
+    STUDENT: 1,
+    TEACHER: 2,
+    HELPER: 3,
+}
+  

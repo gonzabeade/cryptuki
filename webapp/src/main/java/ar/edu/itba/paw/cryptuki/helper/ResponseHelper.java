462ebb8c703d3a90dda295165f45fb61d6df29e0
salvaCasta276 <salCas276@gmail.com>
2022-09-22 13:48:06 -0300

    Fixed paging bounds

--- webapp/src/main/java/ar/edu/itba/paw/cryptuki/helper/ResponseHelper.java
+++ webapp/src/main/java/ar/edu/itba/paw/cryptuki/helper/ResponseHelper.java
@@ -3,6 +3,8 @@ package ar.edu.itba.paw.cryptuki.helper;
 
 import javax.ws.rs.core.*;
 
+import static java.lang.Math.ceil;
+
 public class ResponseHelper {
 
     private ResponseHelper() {
@@ -14,7 +16,7 @@ public class ResponseHelper {
     // TODO: decide whether the pageSize should be constantly repeated
     public static Response.ResponseBuilder genLinks(Response.ResponseBuilder rb, UriInfo uriInfo, int page, int pageSize, long totalCount) {
 
-        int maxPage = (int)(totalCount + pageSize - 1) / pageSize;
+        int maxPage = (int) ceil((float) totalCount / pageSize) - 1;
         if ( page > 0)
             rb.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page-1).queryParam("per_page", pageSize).build(), "prev");
 
@@ -22,7 +24,7 @@ public class ResponseHelper {
             rb.link(uriInfo.getAbsolutePathBuilder().queryParam("page", page+1).queryParam("per_page", pageSize).build(), "next");
 
         // TODO: ask whether should add last and first if already in last and first
-        rb.link(uriInfo.getAbsolutePathBuilder().queryParam("page", 1).queryParam("per_page", pageSize).build(), "first");
+        rb.link(uriInfo.getAbsolutePathBuilder().queryParam("page", 0).queryParam("per_page", pageSize).build(), "first");
         rb.link(uriInfo.getAbsolutePathBuilder().queryParam("page", maxPage).queryParam("per_page", pageSize).build(), "last");
         return rb;
     }

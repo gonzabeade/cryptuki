78c8bdb7ff1097deeba1a6bfb913631a85c19e9b
salvaCasta276 <salCas276@gmail.com>
2022-10-29 17:35:27 -0300

    Added required method to handle refresh token

--- webapp/src/main/java/ar/edu/itba/paw/cryptuki/auth/jwt/JwtUtils.java
+++ webapp/src/main/java/ar/edu/itba/paw/cryptuki/auth/jwt/JwtUtils.java
@@ -18,44 +18,80 @@ import java.util.function.Function;
 public class JwtUtils { // Component that implements serializable?
 
     private static int JWT_TOKEN_VALIDITY = 24 * 60 * 60; // A day
-    @Value("${jwt.secret}")
-    private static String secret = "PAPAYAREPLACETHIS";
+    @Value("${jwt.accessSecret}")
+    private static String accessSecret= "PAPAYAREPLACETHIS";
+
+    @Value("${jwt.refreshSecret}")
+    private static String refreshSecret= "MAMAYAREPLACETHIS";
 
     private JwtUtils() {
     }
 
-    public static Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    public static Claims getAllClaimsFromAccessToken(String token) {
+        return Jwts.parser().setSigningKey(accessSecret).parseClaimsJws(token).getBody();
     }
 
-    public static <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getAllClaimsFromToken(token);
+    public static Claims getAllClaimsFromRefreshToken(String token) {
+        return Jwts.parser().setSigningKey(refreshSecret).parseClaimsJws(token).getBody();
+    }
+
+    public static <T> T getClaimFromAccessToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromAccessToken(token);
         return claimsResolver.apply(claims);
     }
 
-    public static String getUsernameFromToken(String token) {
-        return getClaimFromToken(token, Claims::getSubject);
+    public static <T> T getClaimFromRefreshToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromRefreshToken(token);
+        return claimsResolver.apply(claims);
     }
 
-    public static LocalDate getIssuedAtDateFromToken(String token) {
-        return LocalDate.from((getClaimFromToken(token, Claims::getIssuedAt).toInstant()));
+    public static String getUsernameFromRefreshToken(String token) {
+        return getClaimFromRefreshToken(token, Claims::getSubject);
     }
 
-    public static LocalDate getExpirationDateFromToken(String token) {
-        return LocalDate.from((getClaimFromToken(token, Claims::getExpiration).toInstant()));
+    public static String getUsernameFromAccessToken(String token) {
+        return getClaimFromAccessToken(token, Claims::getSubject);
     }
 
-    private static boolean isTokenExpired(String token) {
-        final LocalDate date = getExpirationDateFromToken(token);
+    public static LocalDate getIssuedAtDateFromAccessToken(String token) {
+        return LocalDate.from((getClaimFromAccessToken(token, Claims::getIssuedAt).toInstant()));
+    }
+
+    public static LocalDate getIssuedAtDateFromRefreshToken(String token) {
+        return LocalDate.from((getClaimFromRefreshToken(token, Claims::getIssuedAt).toInstant()));
+    }
+
+    public static LocalDate getExpirationDateFromAccessToken(String token) {
+        return LocalDate.from((getClaimFromAccessToken(token, Claims::getExpiration).toInstant()));
+    }
+
+    public static LocalDate getExpirationDateFromRefreshToken(String token) {
+        return LocalDate.from((getClaimFromRefreshToken(token, Claims::getExpiration).toInstant()));
+    }
+
+    /////////////////
+
+    private static boolean isAccessTokenExpired(String token) {
+        final LocalDate date = getExpirationDateFromAccessToken(token);
         return date.isBefore(LocalDate.now());
     }
 
-    public static String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return doGenerateToken(claims, userDetails.getUsername());
+    private static boolean isRefreshTokenExpired(String token) {
+        final LocalDate date = getExpirationDateFromRefreshToken(token);
+        return date.isBefore(LocalDate.now());
     }
 
-    private static String doGenerateToken(Map<String, Object> claims, String subject) {
+    public static String generateAccessToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername(), accessSecret);
+    }
+
+    public static String generateRefreshToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername(), refreshSecret);
+    }
+
+    private static String doGenerateToken(Map<String, Object> claims, String subject, String secret) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
@@ -65,12 +101,21 @@ public class JwtUtils { // Component that implements serializable?
                 .compact();
     }
 
-    public static boolean canTokenBeRefreshed(String token) {
-        return !isTokenExpired(token);
+    public static boolean doValidateAccessToken(String token, UserDetails userDetails, String refreshSecret) {
+        final String username = getUsernameFromAccessToken(token);
+        return username.equals(userDetails.getUsername());  // && !isTokenExpired(token); TODO: CHECK!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 
-    public static boolean validateToken(String token, UserDetails userDetails) {
-        final String username = getUsernameFromToken(token);
+    public static boolean doValidateRefreshToken(String token, UserDetails userDetails, String refreshSecret) {
+        final String username = getUsernameFromRefreshToken(token);
         return username.equals(userDetails.getUsername());  // && !isTokenExpired(token); TODO: CHECK!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+    public static boolean validateRefreshToken(String token, UserDetails userDetails){
+        return doValidateRefreshToken(token, userDetails, refreshSecret);
+    }
+
+    public static boolean validateAccessToken(String token, UserDetails userDetails){
+        return doValidateAccessToken(token, userDetails, accessSecret);
     }
 }

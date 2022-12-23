91141db793288a046ed0193101a70d7d11310443
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-23 19:24:27 -0300

    feat: Message conditional styling

--- spa-client/src/components/Message/index.tsx
+++ spa-client/src/components/Message/index.tsx
@@ -7,11 +7,11 @@ type MessageProps = {
 
 const Message:React.FC<MessageProps> = ({left, content}) => {
     return (
-        <li className="flex justify-start">
-            <div className={"relative max-w-xl px-4 py-2 text-gray-700 rounded shadow" + left ? "":"bg-[#eff6e5]" }>
+        <div className="flex justify-start">
+            <div className= {`relative max-w-xl px-4 py-2 text-gray-700 rounded shadow " + ${ left ? '':'bg-[#eff6e5]' }` }>
                 <span className="block">{content}</span>
             </div>
-        </li>
+        </div>
     );
 };
 
--- spa-client/src/views/BuyOffer/index.tsx
+++ spa-client/src/views/BuyOffer/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import UserInfo from "../../components/UserInfo/index";
+import Message from "../../components/Message";
 
 const BuyOffer = () => {
     return (
-       <UserInfo username={"mdedeu"}
-                 email={"mdedeu@itba.edu.ar"}
-                 phone_number={"1234141"}
-                 last_login={"ayer"}
-                 trades_completed={3}
-                 rating={4.9}
-       />
+        <>
+            <UserInfo username={"mdedeu"}
+                      email={"mdedeu@itba.edu.ar"}
+                      phone_number={"1234141"}
+                      last_login={"ayer"}
+                      trades_completed={3}
+                      rating={4.9}
+            />
+            <ul>
+                <Message left={false} content={"messi"}/>
+            </ul>
+        </>
     );
 };
 

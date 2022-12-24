d18900ac1add75deeb2ec2c702fa3e12f30ea509
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-24 16:46:10 -0300

    chore: update TransactionModel.tsx

--- spa-client/src/types/TransactionModel.tsx
+++ spa-client/src/types/TransactionModel.tsx
@@ -1,5 +1,10 @@
+import OfferModel from "./OfferModel";
+
 export default interface TransactionModel {
     status:string,
     icon:JSX.Element
+    buyer:string,
+    seller:string,
+    offer:OfferModel
     //TODO:lo q falte
 }

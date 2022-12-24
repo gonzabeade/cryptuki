d65c1ae91e5900e0da733776f63a9d0b3ed7ca02
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-24 17:19:28 -0300

    feat: User Profile Card

--- spa-client/src/components/UserInfo/index.tsx
+++ spa-client/src/components/UserInfo/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RatingStars from "../RatingStars";
 
 type UserInfoProps = {
     username:string,
@@ -21,23 +22,7 @@ const UserInfo: React.FC<UserInfoProps> = ({username, email, phone_number, last_
                 <span className="text-sm text-gray-400">Last Active: {last_login}</span>
                 <span className="text-sm text-gray-400">Trades Completed: {trades_completed}</span>
 
-                <div className="flex items-center">
-                    { [...Array(Math.trunc(rating))].map((element, key)=>{
-                        return (
-                            <div key={key}>
-                                <svg aria-hidden="true" className="w-5 h-5 text-yellow-400" fill="currentColor"
-                                     viewBox="0 0 20 20"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <title>Star</title>
-                                    <path
-                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                                </svg>
-                            </div>
-                        );})
-                    }
-
-                    <p className="ml-2 text-sm font-medium text-gray-400">{rating} out of 5</p>
-                </div>
+                <RatingStars rating={rating}/>
             </div>
         </div>
 

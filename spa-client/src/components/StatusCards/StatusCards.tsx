997e994b862c956a1822676c82d6d7c5711f0ee2
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-25 16:14:34 -0300

    chore: tailwind theme colors config

--- spa-client/src/components/StatusCards/StatusCards.tsx
+++ spa-client/src/components/StatusCards/StatusCards.tsx
@@ -9,28 +9,28 @@ const StatusCards:React.FC<StatusCardsProps> = ({active}) => {
         <nav className="border-gray-200 py-3 h-20">
             <div className="container mx-auto my-auto px-20 h-full">
                 <ol className="flex justify-center mt-4">
-                    <li className="bg-nyellow rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                    <li className="bg-nyellow rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
                         <a href="/buyer/?status=PENDING">
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active =='pending'?'decoration-frostdr underline underline-offset-8':' '}`}>
                                 Pending
                             </p>
                         </a>
                     </li>
-                    <li className="bg-ngreen rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                    <li className="bg-ngreen rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
                         <a href="/buyer/accepted">
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active =='accepted'?'decoration-frostdr underline underline-offset-8':' '} `}>
                                 Accepted
                             </p>
                         </a>
                     </li>
-                    <li className="bg-nred rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                    <li className="bg-nred rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
                         <a href="/buyer/rejected">
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active == 'rejected'?'decoration-frostdr underline underline-offset-8':' '} `}>
                                 Rejected
                             </p>
                         </a>
                     </li>
-                    <li className="bg-[#FAFCFF] rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                    <li className="bg-[#FAFCFF] rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
                         <a href="/buyer/sold">
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${ active == 'sold'? 'decoration-frostdr underline underline-offset-8':' '}`}>
                                 Sold

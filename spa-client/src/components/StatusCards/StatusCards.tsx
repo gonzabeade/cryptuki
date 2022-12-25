3b6f304bb70c0fab7938aaaeec4ccecc8eb08fab
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-25 16:08:08 -0300

    feat: Profile Filtering status cards

---
+++ spa-client/src/components/StatusCards/StatusCards.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+type StatusCardsProps = {
+    active:string
+}
+
+const StatusCards:React.FC<StatusCardsProps> = ({active}) => {
+    return (
+        <nav className="border-gray-200 py-3 h-20">
+            <div className="container mx-auto my-auto px-20 h-full">
+                <ol className="flex justify-center mt-4">
+                    <li className="bg-nyellow rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                        <a href="/buyer/?status=PENDING">
+                            <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active =='pending'?'decoration-frostdr underline underline-offset-8':' '}`}>
+                                Pending
+                            </p>
+                        </a>
+                    </li>
+                    <li className="bg-ngreen rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                        <a href="/buyer/accepted">
+                            <p className="py-2 pr-4 pl-3 font-bold text-polar ${param.status=='ACCEPTED'?'decoration-frostdr underline underline-offset-8':'text-l '}">
+                                Accepted
+                            </p>
+                        </a>
+                    </li>
+                    <li className="bg-nred rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                        <a href="/buyer/rejected">
+                            <p className="py-2 pr-4 pl-3 font-bold text-polar ${param.status=='REJECTED'?'decoration-frostdr underline underline-offset-8':'text-l '}">
+                                Rejected
+                            </p>
+                        </a>
+                    </li>
+                    <li className="bg-[#FAFCFF] rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200">
+                        <a href="/buyer/sold">
+                            <p className="py-2 pr-4 pl-3 font-bold text-polar ${param.status=='SOLD'?'decoration-frostdr underline underline-offset-8':'text-l '}">
+                                Sold
+                            </p>
+                        </a>
+                    </li>
+                </ol>
+            </div>
+        </nav>
+    );
+};
+
+export default StatusCards;
--- spa-client/src/views/BuyOffer/index.tsx
+++ spa-client/src/views/BuyOffer/index.tsx
@@ -3,7 +3,7 @@ import UserInfo from "../../components/UserInfo/index";
 import Message from "../../components/Message";
 import TradeStatusAlert from "../../components/TradeStatusAlert";
 import {InformationCircleIcon} from "@heroicons/react/24/outline";
-import OfferStatusCard from "../../components/OfferStatusCard/OfferStatusCard";
+import StatusCards from "../../components/StatusCards/StatusCards";
 
 const BuyOffer = () => {
     return (
@@ -19,7 +19,7 @@ const BuyOffer = () => {
             <div className="flex">
                 <TradeStatusAlert icon={<InformationCircleIcon className={`h-10 w-10 text-[#816327]`}/>} color={"#EBCB8B"} accentColor={"#816327"} title={"Your trade proposal was sent to the seller!"} subtitle={"Wait for the seller to accept your trade proposal"}/>
             </div>
-            <OfferStatusCard color={"#EBCB8B"} message={"Pending"} active={true}/>
+            <StatusCards active={'pending'}/>
         </>
     );
 };

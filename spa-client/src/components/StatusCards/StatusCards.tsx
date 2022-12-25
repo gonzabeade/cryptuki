be142d23210bee94bef379920349f0f6dc5d21c9
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-25 17:44:37 -0300

    feat: Base url for Status Component

--- spa-client/src/components/StatusCards/StatusCards.tsx
+++ spa-client/src/components/StatusCards/StatusCards.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
 
 type StatusCardsProps = {
-    active:string
+    active:string,
+    base_url:string
 }
 
-const StatusCards:React.FC<StatusCardsProps> = ({active}) => {
+const StatusCards:React.FC<StatusCardsProps> = ({active, base_url}) => {
     return (
         <nav className="border-gray-200 py-3 h-20">
             <div className="container mx-auto my-auto px-20 h-full">
                 <ol className="flex justify-center mt-4">
                     <li className="bg-nyellow rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
-                        <a href="/buyer/?status=PENDING">
+                        <a href={`${base_url} +/?status=PENDING  `}>
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active =='pending'?'decoration-frostdr underline underline-offset-8':' '}`}>
                                 Pending
                             </p>
                         </a>
                     </li>
                     <li className="bg-ngreen rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
-                        <a href="/buyer/accepted">
+                        <a href={`${base_url} +/?status=ACCEPTED  `}>
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active =='accepted'?'decoration-frostdr underline underline-offset-8':' '} `}>
                                 Accepted
                             </p>
                         </a>
                     </li>
                     <li className="bg-nred rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
-                        <a href="/buyer/rejected">
+                        <a href={`${base_url} +/?status=REJECTED  `}>
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${active == 'rejected'?'decoration-frostdr underline underline-offset-8':' '} `}>
                                 Rejected
                             </p>
                         </a>
                     </li>
                     <li className="bg-[#FAFCFF] rounded-lg shadow-md p-1 mx-5 hover:-translate-y-1 hover:scale-110 duration-200 hover:cursor-pointer">
-                        <a href="/buyer/sold">
+                        <a href={`${base_url} +/?status=SOLD`}>
                             <p className={`py-2 pr-4 pl-3 font-bold text-polar ${ active == 'sold'? 'decoration-frostdr underline underline-offset-8':' '}`}>
                                 Sold
                             </p>
--- spa-client/src/views/BuyOffer/index.tsx
+++ spa-client/src/views/BuyOffer/index.tsx
@@ -19,7 +19,7 @@ const BuyOffer = () => {
             <div className="flex">
                 <TradeStatusAlert icon={<InformationCircleIcon className={`h-10 w-10 text-[#816327]`}/>} color={"#EBCB8B"} accentColor={"#816327"} title={"Your trade proposal was sent to the seller!"} subtitle={"Wait for the seller to accept your trade proposal"}/>
             </div>
-            <StatusCards active={'pending'}/>
+            <StatusCards active={'pending'} base_url={"/buyer"}/>
         </>
     );
 };

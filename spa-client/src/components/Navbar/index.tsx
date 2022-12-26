28ce19ce77d5049dccc39fe0163792520cfd89eb
marquitos.eth <mdedeu@itba.edu.ar>
2022-12-26 00:12:15 -0300

    style: not yet spacing in navbar

--- spa-client/src/components/Navbar/index.tsx
+++ spa-client/src/components/Navbar/index.tsx
@@ -12,45 +12,48 @@ const Navbar = () => {
     const [username, setUsername] = useState<string | null>(userService.getLoggedInUser()); 
 
     return (
-        <nav className="navbar">
-            <Link to="/">
-                <img src={icons.logo} alt="cryptuki logo"/>
-            </Link>
-            <div className="navbar--options">
-                <Link to="/" className="hover:cursor-pointer">
-                    <h1>Mercado P2P</h1>
+        <div className="flex w-full bg-white p-7">
+            <nav className="flex flex-row">
+                <Link  className="my-auto" to="/">
+                    <img src={icons.logo} alt="cryptuki logo"/>
                 </Link>
-                <Link to="/contact" className="hover:cursor-pointer">
-                    <h1>Contáctate</h1>
-                </Link>
-                {
-                    !username && 
-                    <div className='user-holder'>
+                <div className="flex flex-row my-auto mx-auto justify-between">
+                    <Link to="/" className="hover:cursor-pointer justify-around">
+                        <h1>Mercado P2P</h1>
+                    </Link>
+                    <Link to="/contact" className="hover:cursor-pointer">
+                        <h1>Contáctate</h1>
+                    </Link>
+                    {
+                        !username &&
+                        <div >
                             <Link to="/login">
                                 <button className="gray bold">Inicia sesión</button>
                             </Link>
                             <Link to="/register">
                                 <button className="dark bold">Regístrate</button>
                             </Link>
-                    </div> 
-                }
-                {
-                    username && 
-                    <div className='user-holder'>
-                        <Link to="/seller">
-                            <button className="gray bold">Panel de vendedor</button>
-                        </Link>
-                        <Link onClick={ () => {setUsername(userService.getLoggedInUser())}} to="/buyer">
-                            <div className="profile-icon">
-                                <img src={icons.profile} alt="profile icon"/>
-                                <p>{username}</p>
-                            </div>
-                        </Link>
-                        <img className="logout" onClick={() => {localStorage.removeItem("refreshToken"); localStorage.removeItem("accessToken"); setUsername(null); } } src={icons.logout} alt="logout"/>
-                    </div> 
-                }
-            </div>
-        </nav>
+                        </div>
+                    }
+                    {
+                        username &&
+                        <div>
+                            <Link to="/seller">
+                                <button className="gray bold">Panel de vendedor</button>
+                            </Link>
+                            <Link onClick={ () => {setUsername(userService.getLoggedInUser())}} to="/buyer">
+                                <div>
+                                    <img src={icons.profile} alt="profile icon"/>
+                                    <p>{username}</p>
+                                </div>
+                            </Link>
+                            <img onClick={() => {localStorage.removeItem("refreshToken"); localStorage.removeItem("accessToken"); setUsername(null); } } src={icons.logout} alt="logout"/>
+                        </div>
+                    }
+                </div>
+            </nav>
+        </div>
+
     )
 
 }; 

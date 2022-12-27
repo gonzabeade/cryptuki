f723657eea8724c279d4ed2b7283c0db097e0056
Gonza Beade <gbeade@itba.edu.ar>
2022-12-27 18:51:40 +0100

    Implement 50X Mappers

---
+++ webapp/src/main/java/ar/edu/itba/paw/cryptuki/mapper/DefaultExceptionMapper.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.paw.cryptuki.mapper;
+
+import ar.edu.itba.paw.cryptuki.dto.GenericErrorDto;
+import ar.edu.itba.paw.exception.NoSuchCryptocurrencyException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final String MESSAGE = "Internal server error - Cause unknown";
+
+    @Override
+    public Response toResponse(Exception e) {
+        GenericErrorDto dto = GenericErrorDto.fromMessage(MESSAGE);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(dto).build();
+    }
+}

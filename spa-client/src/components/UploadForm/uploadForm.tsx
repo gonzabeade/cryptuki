7beec374e5ca06de52e97c7b60726f849e09594a
marquitos.eth <mdedeu@itba.edu.ar>
2023-01-03 20:46:53 -0300

    feat: form validations

--- spa-client/src/components/UploadForm/uploadForm.tsx
+++ spa-client/src/components/UploadForm/uploadForm.tsx
@@ -34,7 +34,6 @@ const UploadForm = () => {
     }
     //TODO: This feels like a bad practice. Maybe useEffect on selected value?
     function changeSuggestedPrice(){
-         console.log("entered")
         const selectCryptos:HTMLSelectElement = document.getElementById("cryptoSelected")! as HTMLSelectElement;
         const cryptoModel:CryptocurrencyModel = cryptocurrencies.find(cryptocurrency=> cryptocurrency.code ===  selectCryptos.value)!;
         const price = document.getElementById("priceCrypto") as HTMLElement;
@@ -46,7 +45,7 @@ const UploadForm = () => {
     },[])
 
     //Form
-    const { register, handleSubmit, formState: { errors } } = useForm<UploadFormValues>();
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm<UploadFormValues>();
 
     function onSubmit(data:UploadFormValues) {
         console.log(data);
@@ -62,9 +61,11 @@ const UploadForm = () => {
                         <div className="flex flex-col justify-center">
                             <label
                                 className="text-lg font-sans text-polard  mb-3 mt-2 text-center">Cryptocurrency*</label>
-                            <div className="flex flex-row justify-center mx-auto">
+                            <div className="flex flex-col justify-center mx-auto">
                                 <select className="rounded-lg p-3" id="cryptoSelected"
-                                        {...register("cryptoCode",{required:true, onChange:changeSuggestedPrice})} defaultValue={"DEFAULT"}>
+                                        {...register("cryptoCode",{required:"You must choose a cryptocurrency to sell", validate:{
+                                            notDefault: value => value !== "DEFAULT" || "You must choose a cryptocurrency to sell"
+                                            }, onChange:changeSuggestedPrice})} defaultValue="DEFAULT">
                                     <option disabled value="DEFAULT">Choose an option</option>
                                     {
                                         cryptocurrencies.map((cryptocurrency)=>{
@@ -76,6 +77,7 @@ const UploadForm = () => {
                                         })
                                     }
                                 </select>
+                                {errors && errors.cryptoCode && <p className="text-red-600">{errors.cryptoCode.message}</p> }
                             </div>
                             <h1 className="flex flex-row mx-auto mt-4">
                                 <p className="text-sm text-gray-400 mr-2">*Suggested Price </p>
@@ -88,9 +90,10 @@ const UploadForm = () => {
                             <div className="flex flex-col justify-center ">
                                 <input type="number" className="h-10 justify-center rounded-lg p-3 mx-auto "
                                        step=".01"
-                                       {...register("unitPrice", {required:true})}
+                                       {...register("unitPrice", {required:"You must set a price per unit", min: {value:100, message:"Please input an amount greater than 100 ARS"}})}
                                 />
                             </div>
+                            {errors && errors.unitPrice && <p className="text-red-600 mx-auto">{errors.unitPrice.message}</p> }
                         </div>
                         <div className="flex flex-col justify-center mt-4">
                             <h2 className="text-lg font-sans text-polard mb-3 text-center flex flex-row justify-center ">Limits*</h2>
@@ -103,10 +106,17 @@ const UploadForm = () => {
                                     <div className="flex flex-row justify-center mx-auto">
                                         <input type="number" className="h-10 justify-center rounded-lg p-3 mx-5 w-20"
                                                step=".00000001"
-                                               {...register("minInCrypto", {required:true})}/>
+                                               {...register("minInCrypto", {
+                                                   required: "Minimum amount is required",
+                                                   min: {value: 0, message: "Please input an amount greater than 0"},
+                                                   validate: {
+                                                       smallerThanMax: value => value <= getValues().maxInCrypto || "Min must be smaller than max",
+                                                   }
+                                               })}/>
                                     </div>
+                                    {errors && errors.minInCrypto && <p className="text-red-600">{errors.minInCrypto.message}</p> }
                                 </div>
-                                <div className="my-5">
+                                <div className="mt-12">
                                     -
                                 </div>
                                 <div>
@@ -117,8 +127,15 @@ const UploadForm = () => {
                                     <div className="flex flex-row justify-center mx-auto">
                                         <input type="number" className="h-10 justify-center rounded-lg p-3 mx-5 w-20"
                                                step=".00000001"
-                                               {...register("maxInCrypto", {required:true})}/>
+                                               {...register("maxInCrypto", {
+                                                   required: "Maximum amount is required",
+                                                   min: {value: 0, message: "Please input an amount greater than 0"},
+                                                   validate: {
+                                                       biggerThanMin: value => value >= getValues().minInCrypto || "Max must be bigger than min",
+                                                   }
+                                               })}/>
                                     </div>
+                                    {errors && errors.maxInCrypto && <p className="text-red-600">{errors.maxInCrypto.message}</p> }
                                 </div>
                             </div>
                         </div>
@@ -130,7 +147,12 @@ const UploadForm = () => {
                             Location</label>
                         <div className="flex flex-col justify-center px-5">
                             <h2 className="text-lg font-sans text-polard text-center flex flex-row justify-center my-3">Neighborhood*</h2>
-                            <select className="font-sans text-polard mb-3 text-center rounded-lg p-2 " {...register("location",{required:true})} defaultValue={"DEFAULT"}>
+                            <select className="font-sans text-polard mb-3 text-center rounded-lg p-2 " {...register("location",{
+                                required: "Location must not be empty",
+                                validate:{
+                                    locationNotEmpty: value => value !== "DEFAULT" || "Must select location"
+                                }
+                            })}  defaultValue="DEFAULT">
                                 <option disabled value="DEFAULT">Choose option</option>
                                 { NEIGHBORHOODS.map((neighborhood)=>{
                                     return (
@@ -140,6 +162,7 @@ const UploadForm = () => {
                                     );
                                 })}
                             </select>
+                            {errors && errors.location && <p className="text-red-600 mx-auto">{errors.location.message}</p> }
                         </div>
                     </div>
                     <div className="flex flex-col px-10 w-1/3">
@@ -149,8 +172,9 @@ const UploadForm = () => {
                             trade proposal </h2>
                         <div className="flex flex-row justify-center w-80 mx-auto mt-2">
                             <textarea className="w-full h-36 rounded-lg mx-auto p-5"
-                                      {...register("automaticResponse")}/>
+                                      {...register("automaticResponse", {maxLength:240})}/>
                         </div>
+                        {errors && errors.automaticResponse && <p className="text-red-600">{errors.automaticResponse.message}</p> }
                     </div>
                 </div>
                 <div className="flex flex-row p-5 mx-auto">

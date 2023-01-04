044774767c768fadc98c1d9bb443c99bc17a3222
marquitos.eth <mdedeu@itba.edu.ar>
2023-01-04 11:08:37 -0300

    feat: Seller Offer Dashboard

--- spa-client/src/views/SellerOfferDashboard/index.tsx
+++ spa-client/src/views/SellerOfferDashboard/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import OfferCardProfile from "../../components/OfferCardProfile";
 import OfferModel from "../../types/OfferModel";
 import Paginator from "../../components/Paginator";
+import TransactionModel from "../../types/TransactionModel";
+import OfferInformationForSeller from "../../components/OfferInformationForSeller";
 
-type SellerOfferDashboardProps ={
-    offer:OfferModel,
-    selected:string
-}
-const SellerOfferDashboard:React.FC<SellerOfferDashboardProps> = ({offer, selected}) => {
+const SellerOfferDashboard = () => {
+
+    const [selectedStatus, setSelectedStatus] = useState<string>("all");
+    const [trades, setTrades] = useState<TransactionModel[]>([]);
+    const [offer, setOffer] = useState<OfferModel>();
+
+    useEffect(()=>{
+        //fetch trade
+    }, [trades])
+
+    useEffect(()=>{
+        //fetch offer
+    }, [offer])
+
+    function changeStatus(status:string){
+        //fetch to our service that asks for trades in specific state
+        setSelectedStatus(status);
+    }
+
+
     return (<>
             <div className="flex flex-row h-full w-full px-20 my-10">
                 <div className="flex flex-col h-3/5 w-1/5 pr-2">
@@ -16,66 +33,70 @@ const SellerOfferDashboard:React.FC<SellerOfferDashboardProps> = ({offer, select
                             Trade proposals received
                         </h1>
                     </div>
-                    <OfferCardProfile offer={offer}/>
-
+                    {offer && <OfferCardProfile offer={offer}/>}
                     <a href="/"
                        className="rounded-lg bg-frost py-3 px-5 text-l font-sans text-center text-white cursor-pointer shadow-lg">
                         Back
                     </a>
                 </div>
                 <div className="flex flex-col w-4/5">
-                    {/*TODO componente */}
                     <div className="flex flex-row  rounded-lg px-5 rounded-lg  justify-between">
                         <div
                             className="flex mr-5 bg-white rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto w-full ">
-                                <p className={`py-2 px-4 font-bold text-polar text-center ${selected === 'all' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
+                            <button  className="my-auto mx-auto w-full " onClick={()=>changeStatus('all')}>
+                                <p className={`py-2 px-4 font-bold text-polar text-center ${selectedStatus === 'all' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
                                     All
                                 </p>
-                            </a>
+                            </button>
                         </div>
                         <div
                             className=" flex mr-5 bg-nyellow rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto">
-                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selected === 'pending' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
+                            <button onClick={()=>{changeStatus('pending')}} className="my-auto mx-auto">
+                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selectedStatus === 'pending' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
                                     Pending</p>
-                            </a>
+                            </button>
                         </div>
                         <div
                             className=" flex mr-5 bg-ngreen rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto">
-                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selected === 'accepted' ? 'decoration-frostdr underline underline-offset-8' : 'text-l'}`}>
-                                    Accepted</p>
-                            </a>
+                            <button onClick={()=>{changeStatus('accepted')}} className="my-auto mx-auto">
+                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selectedStatus === 'accepted' ? 'decoration-frostdr underline underline-offset-8' : 'text-l'}`}>
+                                    Ongoing</p>
+                            </button>
                         </div>
                         <div
                             className=" flex mr-5 bg-nred rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto">
-                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selected === 'rejected' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
-                                    Rejected</p>
-                            </a>
+                            <button onClick={()=>changeStatus('rejected')} className="my-auto mx-auto">
+                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selectedStatus === 'rejected' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
+                                    Rejected by you</p>
+                            </button>
                         </div>
                         <div
                             className=" flex mr-5 bg-gray-200 rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto">
-                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selected === 'sold' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
-                                    Sold </p>
-                            </a>
+                            <button onClick={()=>changeStatus('sold')} className="my-auto mx-auto">
+                                <p className={`py-2 px-4 font-bold text-polar  text-center ${selectedStatus === 'sold' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
+                                    Closed </p>
+                            </button>
                         </div>
                         <div
                             className=" flex mr-5 bg-blue-400 rounded-lg shadow-md py-1 w-full hover:-translate-y-1 hover:scale-110 duration-200 h-16">
-                            <a href="/" className="my-auto mx-auto">
-                                <p className={`py-2  px-4 font-bold text-polar text-center ${selected === 'deleted' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
-                                    Deleted by you</p>
-                            </a>
+                            <button onClick={()=>{changeStatus('deleted')}} className="my-auto mx-auto">
+                                <p className={`py-2  px-4 font-bold text-polar text-center ${selectedStatus === 'deleted' ? 'decoration-frostdr underline underline-offset-8' : 'text-l '}`}>
+                                    Deleted by buyer</p>
+                            </button>
                         </div>
                     </div>
                     <div className="flex flex-col">
                         <div className="flex flex-wrap pl-3 mt-10">
-                            {/*TODO foreach de trades, con el componente de OfferInformationForSeller*/}
+                            {trades && trades.map((trade)=>{
+                                return(
+                                    <OfferInformationForSeller trade={trade} chat={true} key={trade.id}/>
+                                );
+                            })}
                         </div>
+
                         <h1 className="mx-auto text-center">
-                            <Paginator totalPages={10} actualPage={1} callback={() => console.log("a")}/>
+                            {trades.length>0 ?  <Paginator totalPages={10} actualPage={1} callback={() => console.log("a")}/>: <h1 className="text-polar font-bold"> No trade proposals related to this offer</h1>}
+
                         </h1>
                     </div>
                 </div>

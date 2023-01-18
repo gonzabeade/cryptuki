01599477def0f95b8186f076d2f940ef945f7e81
salvaCasta276 <salCas276@gmail.com>
2023-01-18 12:23:51 -0300

    Finished basic version of kyc view

--- spa-client/src/components/KycForm/kycForm.tsx
+++ spa-client/src/components/KycForm/kycForm.tsx
@@ -4,183 +4,113 @@ import useCryptocurrencyService from "../../hooks/useCryptocurrencyService";
 import {NEIGHBORHOODS} from "../../common/constants";
 import {toast} from "react-toastify";
 import {useForm} from "react-hook-form";
+import {UploadFormValues} from "../UploadForm/uploadForm";
 
 export interface UploadKycValues {
-    minInCrypto:number,
-    maxInCrypto:number,
-    location:string,
-    unitPrice:number,
-    cryptoCode:string,
-    comments?:string
+    names:string,
+    surnames:string,
+    emissionCountry:string,
+    documentNumber:number,
+    idType:string,
+    idPicture:File,
+    facePicture:File
 }
 
 const KycForm = () => {
 
-    //State
-    const [cryptocurrencies, setCryptoCurrencies] = useState<CryptocurrencyModel[]>([]);
-    const cryptocurrencyService = useCryptocurrencyService();
-
-     async function fetchCryptocurrencies(){
-         try{
-             const apiCall:CryptocurrencyModel[] = await cryptocurrencyService.getCryptocurrencies();
-             setCryptoCurrencies(apiCall);
-         }catch (e) {
-             toast.error("Connection error. Failed to fetch cryptocurrencies");
-         }
-    }
-    //TODO: This feels like a bad practice. Maybe useEffect on selected value?
-    function changeSuggestedPrice(){
-        const selectCryptos:HTMLSelectElement = document.getElementById("cryptoSelected")! as HTMLSelectElement;
-        const cryptoModel:CryptocurrencyModel = cryptocurrencies.find(cryptocurrency=> cryptocurrency.code ===  selectCryptos.value)!;
-        const price = document.getElementById("priceCrypto") as HTMLElement;
-        price.innerHTML = cryptoModel?.price ? cryptoModel?.price.toString() + ' ARS': 'No price detected' ;
-    }
-
-    useEffect(()=>{
-        fetchCryptocurrencies();
-    },[])
-
     //Form
     const { register, handleSubmit, formState: { errors }, getValues } = useForm<UploadKycValues>();
 
-    function onSubmit(data:UploadKycValues) {
+    function onSubmit(data:UploadFormValues) {
         console.log(data);
     }
 
+    //TODO HTML
+    //-Mirar como encontrar un valor para el texto entre xl y lg
+    //-Mirar como hacer para que el form se expanda a toda la pantalla
+    //-Mirar como poner el enctype="multipart/form-data" en los inputs de archivos y si es necesario
+
     return (
         <div className="flex flex-row mx-auto">
-            <form className="flex flex-col min-w-[50%]" onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-row divide-x">
-                    <div className="flex flex-col mx-5 w-1/3">
-                        <h1 className="font-sans text-polar font-bold text-xl text-center">1. Price
-                            Settings </h1>
-                        <div className="flex flex-col justify-center">
-                            <label
-                                className="text-lg font-sans text-polard  mb-3 mt-2 text-center">Cryptocurrency*</label>
-                            <div className="flex flex-col justify-center mx-auto">
-                                <select className="rounded-lg p-3" id="cryptoSelected"
-                                        {...register("cryptoCode",{required:"You must choose a cryptocurrency to sell", validate:{
-                                            notDefault: value => value !== "DEFAULT" || "You must choose a cryptocurrency to sell"
-                                            }, onChange:changeSuggestedPrice})} defaultValue="DEFAULT">
-                                    <option disabled value="DEFAULT">Choose an option</option>
-                                    {
-                                        cryptocurrencies.map((cryptocurrency)=>{
-                                           return (
-                                               <option value={cryptocurrency.code} key={cryptocurrency.code}>
-                                                   {cryptocurrency.commercialName}
-                                               </option>
-                                           );
-                                        })
-                                    }
+            <form className="flex flex-col w-1/2 mt-20 mb-10 py-12 px-4 justify-start mx-auto rounded-lg bg-stormd/[0.9] border-2 border-polard">
+                <div className="flex">
+                    <div className="flex flex-col mx-auto">
+                        <h1 className="font-sans font-semibold text-polar text-2xl text-center">
+                            Verify your Identity
+                        </h1>
+                        <h2 className="text-start text-lg font-semibold font-sans text-polar mt-8">
+                            Each seller must verify it's identity. We will get back to you by mail after verifying the information in this form
+                        </h2>
+                        <h2 className="text-start text-xl font-semibold font-sans text-polar underline mt-8">
+                            Section 1 : Passport/ID information
+                        </h2>
+                        <h2 className="text-start text-lg font-semibold font-sans text-polar mt-2">
+                            Input the personal data regarding this passport/id.
+                        </h2>
+                        <h2 className="text-start text-lg font-semibold font-sans text-polar mt-2">
+                            If your country is not present on this list, we can't verify your information yet!
+                        </h2>
+                        <div className="flex flex-col mx-5 mt-4 items-start">
+                            <div className="flex flex-col mr-10 my-4 w-1/2">
+                                <label className="text-start text-xl font-bold font-sans text-polar my-2">Names</label>
+                                <input className="p-2 m-2 rounded-lg shadow mx-auto" placeholder="e.g. Pedro Martin"/>
+                            </div>
+                            <div className="flex flex-col mr-10 my-4 w-1/2">
+                                <label className="text-start text-xl font-bold font-sans text-polar my-2">Surnames</label>
+                                <input className="p-2 m-2 rounded-lg shadow mx-auto" placeholder="e.g. Juarez"/>
+                            </div>
+                            <div className="flex flex-col mr-10 my-4 w-1/2">
+                                <label className="text-start text-xl font-bold font-sans text-polar my-2">Emission country</label>
+                                <select className="rounded-lg p-3 bg-white">
+                                    <option value="ARG">Argentina</option>
+                                    <option value="CHL">Chile</option>
+                                    <option value="URY">Uruguay</option>
                                 </select>
-                                {errors && errors.cryptoCode && <p className="text-red-600 mx-auto mt-2">{errors.cryptoCode.message}</p> }
-                            </div>
-                            <h1 className="flex flex-row mx-auto mt-4">
-                                <p className="text-sm text-gray-400 mr-2">*Suggested Price </p>
-                                <p className="text-sm text-gray-400" id="priceCrypto">Select a coin </p>
-                            </h1>
-                        </div>
-                        <div className="flex flex-col mt-4">
-                            <label className="text-lg font-sans text-polard  mb-3 text-center ">Price per unit in
-                                ARS*</label>
-                            <div className="flex flex-col justify-center ">
-                                <input type="number" className="h-10 justify-center rounded-lg p-3 mx-auto "
-                                       step=".01"
-                                       {...register("unitPrice", {required:"You must set a price per unit", min: {value:100, message:"Please input an amount greater than 100 ARS"}})}
-                                />
-                            </div>
-                            {errors && errors.unitPrice && <p className="text-red-600 mx-auto mt-2">{errors.unitPrice.message}</p> }
-                        </div>
-                        <div className="flex flex-col justify-center mt-4">
-                            <h2 className="text-lg font-sans text-polard mb-3 text-center flex flex-row justify-center ">Limits*</h2>
-                            <div className="flex flex-row justify-center">
-                                <div>
-                                    <label
-                                        className="text-sm font-sans text-polard mb-3 text-center flex flex-row justify-center ">
-                                       Min in
-                                        <p id="minCoin" className="mx-2">BTC</p></label>
-                                    <div className="flex flex-row justify-center mx-auto">
-                                        <input type="number" className="h-10 justify-center rounded-lg p-3 mx-5 w-20"
-                                               step=".00000001"
-                                               {...register("minInCrypto", {
-                                                   required: "Minimum amount is required",
-                                                   min: {value: 0, message: "Please input an amount greater than 0"},
-                                                   validate: {
-                                                       smallerThanMax: value => value <= getValues().maxInCrypto || "Min must be smaller than max",
-                                                   }
-                                               })}/>
-                                    </div>
-                                    {errors && errors.minInCrypto && <p className="text-red-600 mx-auto mt-2">{errors.minInCrypto.message}</p> }
-                                </div>
-                                <div className="mt-12">
-                                    -
-                                </div>
-                                <div>
-                                    <label
-                                        className="text-sm font-sans text-polard mb-3 text-center flex flex-row justify-center">
-                                        Max in
-                                        <p id="maxCoin" className="mx-2">BTC</p></label>
-                                    <div className="flex flex-row justify-center mx-auto">
-                                        <input type="number" className="h-10 justify-center rounded-lg p-3 mx-5 w-20"
-                                               step=".00000001"
-                                               {...register("maxInCrypto", {
-                                                   required: "Maximum amount is required",
-                                                   min: {value: 0, message: "Please input an amount greater than 0"},
-                                                   validate: {
-                                                       biggerThanMin: value => value >= getValues().minInCrypto || "Max must be bigger than min",
-                                                   }
-                                               })}/>
-                                    </div>
-                                    {errors && errors.maxInCrypto && <p className="text-red-600 mt-2 mx-auto">{errors.maxInCrypto.message}</p> }
-                                </div>
                             </div>
                         </div>
-
-                    </div>
-                    <div className="flex flex-col mx-auto w-1/3 ">
-                        <label
-                            className="text-xl font-sans text-polar font-bold mb-3 text-center ">2.
-                            Location</label>
-                        <div className="flex flex-col justify-center px-5">
-                            <h2 className="text-lg font-sans text-polard text-center flex flex-row justify-center my-3">Neighborhood*</h2>
-                            <select className="font-sans text-polard mb-3 text-center rounded-lg p-2 " {...register("location",{
-                                required: "Location must not be empty",
-                                validate:{
-                                    locationNotEmpty: value => value !== "DEFAULT" || "Must select location"
-                                }
-                            })}  defaultValue="DEFAULT">
-                                <option disabled value="DEFAULT">Choose option</option>
-                                { NEIGHBORHOODS.map((neighborhood)=>{
-                                    return (
-                                        <option value={neighborhood} key={neighborhood}>
-                                            {neighborhood}
-                                        </option>
-                                    );
-                                })}
-                            </select>
-                            {errors && errors.location && <p className="text-red-600 mx-auto">{errors.location.message}</p> }
+                        <div className="flex flex-row mx-5 mt-4 items-end">
+                            <div className="flex flex-col mr-10 my-4 w-1/2">
+                                <label className="text-start text-xl font-bold font-sans text-polar my-2">
+                                    Document Number
+                                </label>
+                                <input placeholder="e.g. 45089768" type="text" className="rounded-lg p-3"/>
+                            </div>
+                            <div className="flex flex-col mr-10 my-4 w-1/2">
+                                <label className="text-start text-xl font-bold font-sans text-polar my-2">
+                                    Passport/ID Type
+                                </label>
+                                <select className="rounded-lg p-3 bg-white">
+                                    <option value="ID">National Id</option>
+                                    <option value="PASSPORT">Passport</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="flex flex-col mr-18 my-4 w-1/2">
+                            <label className="text-start text-xl font-bold font-sans text-polar my-2">
+                               Passport/ID picture
+                            </label>
+                            <input type="file" className="rounded-lg p-3" accept="image/png, image/gif, image/jpeg"/>
+                        </div>
+                        <h2 className="text-start text-2xl font-semibold font-sans text-polar underline mt-8">
+                            Section 2: Face and Picture data validation
+                        </h2>
+                        <h2 className="text-start text-xl font-semibold font-sans text-polar mt-2">
+                            For safety purposes, to validate you're using your password,  we require you to have a picture taken of yourself, showing half of your body and complete face holding your document.
+                        </h2>
+                        <div className="flex flex-row mx-5 mt-4 items-center">
+                           <div className="flex flex-col my-10 mx-auto">
+                               <div className="flex flex-col mx-5 my-4 px-16">
+                                   <label className="text-center text-xl font-bold font-sans text-polar my-2">
+                                       Upload picture
+                                   </label>
+                                   <input type="file" className="rounded-lg p-3" accept="image/png, image/gif, image/jpeg"/>
+                               </div>
+                           </div>
+                        </div>
+                        <div className="flex flex-row w-full justify-center mt-10">
+                            <input type="submit" value="Send" className="w-1/5 rounded-lg bg-frostl py-3 px-5 text-white cursor-pointer shadow-lg"/>
                         </div>
                     </div>
-                    <div className="flex flex-col px-10 w-1/3">
-                        <label className="text-xl font-sans text-polar font-bold mb-3 text-center ">3. Automatic
-                            Response</label>
-                        <h2 className="text-justify">You can set an automatic response every time someone makes you a
-                            trade proposal </h2>
-                        <div className="flex flex-row justify-center w-80 mx-auto mt-2">
-                            <textarea className="w-full h-36 rounded-lg mx-auto p-5"
-                                      {...register("comments", {maxLength: {value:240, message:"Max length is 240 characters"}})}/>
-                        </div>
-                        {errors && errors.comments && <p className="mt-2 text-red-600 mx-auto">{errors.comments.message}</p> }
-                    </div>
-                </div>
-                <div className="flex flex-row p-5 mx-auto">
-                    <a className="font-bold cursor-pointer bg-polarlr/[0.6] text-white text-center mt-4 p-3 rounded-md font-sans mx-5 w-32"
-                       href="/">Cancel
-                    </a>
-                    <button type="submit"
-                            className=" font-bold bg-frostdr text-white  mt-4 p-3 rounded-md font-sans  w-32 mx-5 active:cursor-progress">
-                        Submit
-                    </button>
                 </div>
             </form>
         </div>
--- spa-client/src/views/UploadKyc/index.tsx
+++ spa-client/src/views/UploadKyc/index.tsx
@@ -6,13 +6,8 @@ const UploadKyc = () => {
     return (
         <div className="flex flex-row divide-x w-full">
             <div className="flex flex-col w-full">
-                <div className="flex">
-                    <h1 className="mx-auto my-10 text-2xl font-semibold font-sans text-polar">
-                       Upload your Kyc
-                    </h1>
-                </div>
                 <div className="flex items-center justify-center">
-                    <UploadKyc/>
+                    <KycForm/>
                 </div>
             </div>
         </div>

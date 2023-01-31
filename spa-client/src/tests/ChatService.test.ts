f11c9529e36ed72c55b64079f621fa980d808b8e
salvaCasta276 <salCas276@gmail.com>
2023-01-31 09:53:39 -0300

    Finished tests of ComplainService

--- spa-client/src/tests/ChatService.test.ts
+++ spa-client/src/tests/ChatService.test.ts
@@ -1,8 +1,6 @@
 import {ChatService} from "../services/ChatService";
-import useChatService from "../hooks/useChatService";
 import axios from "axios";
 import {paths} from "../common/constants";
-import {AxiosService} from "../services/AxiosService";
 
 
 jest.mock('axios')
@@ -10,27 +8,28 @@ jest.mock('axios')
 
 describe("get message", () =>{
     it("makes GET to proper URL", () =>{
-        let chatService = new ChatService(() => axios)
+        const chatService = new ChatService(() => axios)
         axios.get = jest.fn().mockResolvedValue({data: []})
 
         chatService.getMessages(1)
 
-        expect(axios.get).toHaveBeenCalledWith(paths.BASE_URL + paths.TRADE + "1" + "/messages")
+        expect(axios.get).toHaveBeenCalledWith(paths.BASE_URL + paths.TRADE + "1/messages")
         expect(axios.get).toHaveBeenCalledTimes(1)
     })
 })
 
 describe("send message", () =>{
     it("makes POST to proper URL", () =>{
-        let chatService = new ChatService(() => axios)
+        const chatService = new ChatService(() => axios)
         axios.post = jest.fn().mockResolvedValue({data: []})
         const message = "This is a new message"
 
         chatService.sendMessage(1, message)
 
         expect(axios.post).toHaveBeenCalledWith(
-            paths.BASE_URL + paths.TRADE + "1" + "/messages",
-            {"message" : message})
+            paths.BASE_URL + paths.TRADE + "1/messages",
+            {"message" : message}
+        )
         expect(axios.post).toHaveBeenCalledTimes(1)
     })
 })
---
+++ spa-client/src/tests/ComplainService.test.ts
@@ -0,0 +1,87 @@
+import {ComplainService} from "../services/ComplainService";
+import axios from "axios";
+import * as utils from "../common/utils/utils";
+import {paths} from "../common/constants";
+import {CreateComplainForm} from "../views/Support";
+import {SolveComplaintFormModel} from "../components/SolveComplaintForm/SolveComplaintForm";
+import {ChatService} from "../services/ChatService";
+
+jest.mock('axios')
+
+test("get complaints by URL", () => {
+    const complainService = new ComplainService(() => axios)
+    axios.get = jest.fn().mockResolvedValue({data: []})
+    jest.spyOn(utils, 'processPaginatedResults').mockReturnThis();
+    const test_url = "test_url"
+
+    complainService.getComplaintsByUrl(test_url)
+
+    expect(axios.get).toHaveBeenCalledWith(test_url)
+    expect(axios.get).toHaveBeenCalledTimes(1)
+})
+
+test("get complaints", () => {
+    const complainService = new ComplainService(() => axios)
+    axios.get = jest.fn().mockResolvedValue({data: []})
+    jest.spyOn(utils, 'processPaginatedResults').mockReturnThis();
+
+    const params = new URLSearchParams()
+    params.append("status","PENDING");
+
+    complainService.getComplaints()
+
+    expect(axios.get).toHaveBeenCalledWith(paths.BASE_URL + paths.COMPLAINTS, {"params": params})
+    expect(axios.get).toHaveBeenCalledTimes(1)
+})
+
+test("get complaints by id", () => {
+    const complainService = new ComplainService(() => axios)
+    axios.get = jest.fn().mockResolvedValue({data: []})
+
+    complainService.getComplaintById(1)
+
+    expect(axios.get).toHaveBeenCalledWith(paths.BASE_URL + paths.COMPLAINTS + "/1")
+    expect(axios.get).toHaveBeenCalledTimes(1)
+})
+
+test("create complaint", () => {
+    const complaintService = new ComplainService(() => axios)
+    axios.post = jest.fn().mockResolvedValue({data: []})
+    const complainForm : CreateComplainForm = {
+        email : "scastagnino@itba.edu.ar",
+        tradeId : 1,
+        message : "This is a new complaint"
+    }
+
+    complaintService.createComplain(complainForm)
+
+    expect(axios.post).toHaveBeenCalledWith(
+        paths.BASE_URL + paths.COMPLAINTS,
+        {
+            tradeId : complainForm.tradeId,
+            message : complainForm.message
+        }
+    )
+    expect(axios.post).toHaveBeenCalledTimes(1)
+})
+
+test("create complaint resolution", () => {
+    const complaintService = new ComplainService(() => axios)
+    axios.post = jest.fn().mockResolvedValue({data: []})
+    const resolutionForm : SolveComplaintFormModel = {
+        comments : "These are comments",
+        resolution : "This es a resolution",
+        complainId : 1
+    }
+
+    complaintService.createComplainResolution(resolutionForm)
+
+    expect(axios.post).toHaveBeenCalledWith(
+        paths.BASE_URL + paths.COMPLAINTS + "/1/resolution",
+        {
+            resolution : resolutionForm.resolution,
+            comments : resolutionForm.comments
+        }
+    )
+    expect(axios.post).toHaveBeenCalledTimes(1)
+})

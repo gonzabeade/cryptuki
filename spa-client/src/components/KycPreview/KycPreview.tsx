b454bfae660937912a8e68c8b0a1c58ae14cafd8
marquitos.eth <mdedeu@itba.edu.ar>
2023-02-01 00:37:53 -0300

    fix: i18n

--- spa-client/src/components/KycPreview/KycPreview.tsx
+++ spa-client/src/components/KycPreview/KycPreview.tsx
@@ -11,7 +11,7 @@ const KycPreview = ({username,last_login}:data) => {
     return (
         <div className="flex flex-col bg-white shadow rounded-lg p-3 m-5 font-sans font-bold">
             <div className="w-full mt-2 text-xl text-start"><b>{i18n.t('user')}:</b> {username}</div>
-            <div className="w-full mt-2 text-xl text-start"><b>{i18n.t('lastActive')}</b> {last_login.toString().substring(0, 10)}</div>
+            <div className="w-full mt-2 text-xl text-start"><b>{i18n.t('lastTimeActive')}</b> {last_login.toString().substring(0, 10)}</div>
             <div className="mx-auto my-3">
                 <a href={`/admin/kyc/${username}`}
                    className=" text-center pb-2 px-5 pt-2 rounded-lg bg-stormd max-h-14 text-polard my-auto">{i18n.t('see')}</a>
